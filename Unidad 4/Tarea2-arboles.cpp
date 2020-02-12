3abc5f4ded40a937d0f92735da167400ba89b397
rodrigojfc <00053714@uca.edu.sv>
2020-02-12 16:10:53 -0600

    Add: tarea 2 arboles

---
+++ Unidad 4/Tarea2-arboles.cpp
@@ -0,0 +1,12 @@
+//Calcular profundidad en un arbol
+void depth(node* tree, int contador){
+    if(!(*tree)){
+        cout << "Profundidad del arbol:\t0\n";
+    }
+    
+    else{
+        contador ++;
+        (pTree->left, 0);
+        (pTree->right, 0);
+    }
+}
